41eba8af25a85ebe59eb32dbcd214ac2aee1f2e1
jaarabytes <xavierandole78@gmail.com>
2024-07-24 10:46:48 +0300

    feat: sidenav to be added to dashboard layout
    yes

---
+++ src/lib/components/Sidenav.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+import FaHome from 'svelte-icons/fa/FaHome.svelte'
+import FaHandHoldingHeart from 'svelte-icons/fa/FaHandHoldingHeart.svelte'
+import FaChild from 'svelte-icons/fa/FaChild.svelte'
+ import "../../app.css"
+</script>
+
+<div>
+
+<div>
+  <a href="/">
+    <div>
+      <div class="w-6 h-6 inline">
+          <fahome />
+      </div>
+      home
+    </div>
+  </a>
+
+  <a href="/donate" ><div>
+    <div class="w-6 h-6 inline">
+      <FaHandHoldingHeart />
+    </div>
+    Donate
+    </div>
+  </a>
+
+  <a href="/profile">
+   <div>
+    <div class="w-6 h-6 inline">
+      <FaChild />
+    </div>
+      Profile
+     </div>
+  </a>
+
+
+</div>
+
+<div>
+  <a href="/">
+    <div>
+      <div class="w-6 h-6 inline">
+          <fahome />
+      </div>
+      home
+    </div>
+  </a>
+</div>
+
+</div>
