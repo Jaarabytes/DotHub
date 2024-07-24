024b4ec71ee8730f24e7b0a4e65a0b42370d9dff
jaarabytes <xavierandole78@gmail.com>
2024-07-24 10:46:13 +0300

    feat: boilerplate for the homepage
    Boilerplate for the homepage. Finish up later

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+  import { spring } from "svelte/motion";
+    import Layout from "../+layout.svelte";
+    // use tippy js (for tooltips)
+    // add those charts like in wakatime
+</script>
+
+<main>
+  <p>Insert quote here</p>
+
+  <h1 class="font-bold md:text-[40px] text-3xl">Let's delay some deadlines!</h1>
+
+  <form method="POST" use:enhance>
+    <label for='text'>New Task? </label>
+    <input type='text' class="text-black rounded-lg border-slate-500 border-2" required />
+  </form>
+</main>
