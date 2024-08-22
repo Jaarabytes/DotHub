a27f4b9a650b6e25c01000f9e4924f70f5c1912e
jaarabytes <xavierandole78@gmail.com>
2024-08-22 12:35:10 +0300

    clear clutter, successful build
    Run build works successfully, got rid of clutter code. Now to optimize
    further. Bye!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,16 @@
 <script>
+  import { Github } from 'lucide-svelte'
 </script>
 
 <svelte:head>
-  <title>ProcastiMate</title>
+  <title>Dotfilia</title>
 </svelte:head>
 
 <main>
+  <a href="https://dotfilia.vercel.app" target="_blank" class="font-bold text-black">Dotfilia</a>
+  <a href="https://github.com/jaarabytes/dotfilia" target="_blank" class=""><button><Github class="h-6 w-6 inline" /> Star me</button></a>
   <div class="text-center my-10">
-    <h1 class="lg:text-[50px] my-5 text-center text-[45px] font-extrabold ">Sweeten the NOW !</h1>
-    <p class="text-center text-xl">Reduce your stress, and push deadlines further</p>
-  </div>
-
-  <div class="text-center my-10">
-    <a href="/sign_up"><button class="rounded-lg m-3 text-xl px-5 py-3 bg-violet-800 text-white hover:bg-violet-600">
-        Get ProcastiMate
-        <div class="w-5 h-5 inline-block">
-        </div>
-      </button></a>
-  </div>
-
-  <div class="my-6">
-    <p class="text-center">No need for Virtual Assistants, We help you <span class="font-bold">procastinate</span> your tasks more efficiently. </p>
-  </div>
-
-  <div class="px-3 my-13 md:text-center">
-    <h2 class="my-7 font-extrabold lg:text-[30px] text-2xl ">But procastination is bad ...</h2>
-    <h1 class="my-5 text-xl font-bold">Perfection is our buddy</h1>
-    <p class="text-sm my-5">Good enough is better than best, and faster too!</p>
-    
-    <h1 class="my-7 text-xl font-bold">Delay gratification, enjoy life!</h1>
-    <p class="my-5 text-sm">Postpone tasks, savor the small wins and everyday pleasures</p>
-
-    <h1 class="my-7 text-xl font-bold">Deadline detox</h1>
-    <p class="my-5 text-sm">Embrace the art of putting things off</p>
-
-    <h1 class="my-7 text-xl font-bold">Tick Tock Tomorrow!</h1>
-    <p class="my-5 text-sm">Why rush when you can relax today and enjoy tomorrow</p>
-
-  </div>
-
-  <h2 class="font-bold text-center my-10 text-2xl">ProcastiMate is trusted all over Kenya and beyond</h2>
-  <hr />
-  <div class="text-center mt-10">
+    <h1 class="my-5 text-center text-3xl font-extrabold ">Discover the best dotfiles</h1>
+    <p class="text-center text-md text-slate-600">Dotfilia is a place to find, sort and steal the best dotfiles you can get</p>
   </div>
 </main>
--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,5 +1,40 @@
 <script>
-  import Donate from '$lib/components/Donate.svelte'
+  // implement paystack
+  import "../../app.css"
+  import { onMount } from "svelte";
+
+  let amount = ''
+  function payWithPaystack () {
+    let handler = PaystackPop.setup({
+      // this is my public key. Why not just import the env? Svelte brought up some issues
+      key: "pk_live_9999378c83331abc0c642f96d2457a5f88969934",
+      email: 'xh3rking96@gmail.com',
+      amount: Number(amount) * 100,
+      currency: "KES",
+      callback: function() {
+        alert("Thank you so much!!!");
+      },
+      onClose: function () {
+        alert("NOOO, not yet!!!")
+      }
+    });
+
+    handler.openIframe();
+  }
+
+  onMount(() => {
+    const script= document.createElement('script');
+    script.src = 'https://js.paystack.co/v1/inline.js'
+    document.head.appendChild(script);
+  })
 </script>
 
-<Donate />
+<main class='my-5'>
+  <h1 class='font-extrabold text-center text-3xl'>Be blessed!!!</h1>
+  <form class="text-center my-3" >
+    <label for='amount' class='text-center'>Amount (in KES)</label><br /> <br />
+    <input bind:value={amount} class='text-center rounded-lg text-black py-2 px-3 border-2 border-black' required /><br /><br />
+    <button class="bg-green-900 px-3 py-2 rounded-lg text-white hover:bg-green-700" type='button' on:click={payWithPaystack}>Donate</button>
+  </form>
+  <p class='text-center my-5'>Thank you <a class='font-bold' href='https://paystack.com'>Paystack</a></p>
+</main>
